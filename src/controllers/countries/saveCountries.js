681c87b943cc5b4fd049d372b046e682db286af7
Sofía Chiabudini <sofiachiabudini@gmail.com>
2023-08-02 22:26:08 -0300

    Update saveCountries.js

--- src/controllers/countries/saveCountries.js
+++ src/controllers/countries/saveCountries.js
@@ -9,7 +9,7 @@ const saveCountries = async (Country) => {
             return{
                 id: country.cca3,
                 name: country.name.common,
-                flag: country.flags && country.flags.length ? country.flags[0] : 'No tiene bandera',
+                flag: country.flags && country.flags.png ? country.flags.png : 'No tiene bandera',
                 continent: country.continents[0],
                 capital: country.capital && country.capital[0] ? country.capital[0] : 'No tiene capital',
                 subregion: country?.subregion,
